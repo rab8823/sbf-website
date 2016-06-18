3c853e6ed280a3ce32fe0889e8ad0efb25c24bd2
Andrew Barefoot <rab8823@gmail.com>
2016-06-18 09:42:13 -0400

    Testing mailgun

--- controllers/test.js
+++ controllers/test.js
@@ -24,23 +24,4 @@ exports.testPost = function(req, res) {
             user:process.env.MAILGUN_USERNAME,
             pass:process.env.MAILGUN_PASSWORD,
         };
-    var transporter = nodemailer.createTransport({
-        service: 'Mailgun',
-        auth: auth
-      });
-      console.dir(auth);
-      var mailOptions = {
-        to: 'apolloman3@gmail.com',
-        from: 'support@mail.sunbrookfarm.com',
-        subject: '✔ Reset your password on Mega Boilerplate',
-        text: 'You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n'
-      };
-      transporter.sendMail(mailOptions, function(err) {
-          console.log('sent mail');
-          console.dir(err);
-        req.flash('info', { msg: 'An email has been sent to ' + mailOptions.to + ' with further instructions.' });
-        res.redirect('/');
-      });
-
-  
 };
---
+++ public/js/test.ts
@@ -0,0 +1,6 @@
+class Foo{
+    private bar:number =-1;
+    constructor(){
+        this.bar = 2;
+    }
+}
